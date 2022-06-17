10221c2e5906487573a38285744f28686c6c9260
sam88khang <sam88khang@gmail.com>
2022-06-17 11:21:25 +0100

    Initial Commit

---
+++ First Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Project
+//
+//  Created by Sam on 17/06/2022.
+//  Copyright © 2022 Sam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First ProjectTests/First_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_ProjectTests.swift
+//  First ProjectTests
+//
+//  Created by Sam on 17/06/2022.
+//  Copyright © 2022 Sam. All rights reserved.
+//
+
+import XCTest
+@testable import First_Project
+
+class First_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
